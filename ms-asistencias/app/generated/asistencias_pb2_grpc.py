59d38a8c190a0db22db80da388e05225c99cd00f
Nelson <sosafco3@gmail.com>
2026-05-14 22:24:40 -0600

    Conexion: Conexion inicial entre todos los microservicios existentes

--- ms-asistencias/app/generated/asistencias_pb2_grpc.py
+++ ms-asistencias/app/generated/asistencias_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import asistencias_pb2 as asistencias__pb2
+from . import asistencias_pb2 as asistencias__pb2
 
 GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
